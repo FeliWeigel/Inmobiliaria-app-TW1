8df63fea51f0ffc60a5d9c7e6ef1e9def4f09700
Ezequiel Miranda <mirandaezequiel522@gmail.com>
2024-05-08 22:48:35 -0300

    Refactor 2 de ServicioPropiedad y ControladorPropiedad

--- src/main/java/com/tallerwebi/presentacion/ControladorPropiedad.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPropiedad.java
@@ -2,6 +2,7 @@ package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.Propiedad;
 import com.tallerwebi.dominio.ServicioPropiedad;
+import com.tallerwebi.dominio.excepcion.CRUDPropiedadExcepcion;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,22 @@ public class ControladorPropiedad {
     @GetMapping("/propiedad/{id}")
     public ModelAndView verPropiedad(@PathVariable Long id) {
 
-        return servicioPropiedad.buscarPropiedad(id);
+        ModelMap model = new ModelMap();
+
+         Propiedad propiedad = new Propiedad(1L, "Casa 1", 2, 3, 4, 200.0, 150000.0, "Ubicacion 1");
+
+        try {
+            //Propiedad propiedad = servicioPropiedad.buscarPropiedad(id);
+            model.put("message", "Detalles de la Propiedad.");
+            model.put("propiedad", propiedad);
+            return new ModelAndView("propiedad", model);
+        } catch (CRUDPropiedadExcepcion e) {
+            model.put("message", e.getMessage());
+            return new ModelAndView("propiedad", model);
+        } catch (Exception e) {
+            model.put("message", "Error al Mostrar la Propiedad.");
+            return new ModelAndView("propiedad", model);
+        }
     }
 
 
