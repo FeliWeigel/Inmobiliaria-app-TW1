def15b5003983f7a44ffe1b4bd80bc4e0412f303
FeliWeigel <fweigeldev@gmail.com>
2024-06-10 23:29:45 -0300

    Implementacion base calendario alquiler

--- src/main/webapp/resources/core/js/alquiler.js
+++ src/main/webapp/resources/core/js/alquiler.js
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 const endDate = selectedDates[1];
                 document.getElementById('start_date').value = flatpickr.formatDate(startDate, "d-m-Y");
                 document.getElementById('end_date').value = flatpickr.formatDate(endDate, "d-m-Y");
+                const rangoFechasTexto = document.getElementById('rango-fechas-texto');
+                rangoFechasTexto.innerHTML = `de: ${flatpickr.formatDate(startDate, "d-m-Y")} hasta: ${flatpickr.formatDate(endDate, "d-m-Y")}`;
+
+
             }
         }
     });
