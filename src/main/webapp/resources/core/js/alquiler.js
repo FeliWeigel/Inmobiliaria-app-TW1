58f9dad9c4d30016accbe0a8bbc9a7e0316fcaf4
FeliWeigel <fweigeldev@gmail.com>
2024-06-13 13:06:50 -0300

    Funcionalidad base marcar fechas reservadas en calendario

--- src/main/webapp/resources/core/js/alquiler.js
+++ src/main/webapp/resources/core/js/alquiler.js
@@ -5,44 +5,45 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(fetchURL)
         .then(res => res.json())
         .then(data => {
-            let fechasArray = new Array()
+            let fechasArray = []
             if(data){
                 data.forEach((fechasAlquiler) => {
-                    let fechaInicio = new Date(fechasAlquiler.fechaInicio).toLocaleDateString()
-                    let fechaFin = new Date(fechasAlquiler.fechaFin).toLocaleDateString()
+                    let fechaInicio = new Date(fechasAlquiler.fechaInicio);
+                    let fechaFin = new Date(fechasAlquiler.fechaFin)
 
                     let rangoFechas = {
                         fechaInicio: fechaInicio,
                         fechaFin: fechaFin
                     }
-
                     fechasArray.push(rangoFechas)
                 })
             }
-
-            console.log(fechasArray)
+            mostrarCalendario(fechasArray)
         })
         .catch(err => console.log(err))
 
     // calendario
-    flatpickr("#rango-fechas", {
-        mode: "range",
-        dateFormat: "Y-m-d",
-        inline: true,
-        locale: {
-            rangeSeparator: ' a: '
-        },
-        onChange: function(selectedDates) {
-            if (selectedDates.length === 2) {
-                const startDate = selectedDates[0];
-                const endDate = selectedDates[1];
-                document.getElementById('start_date').value = flatpickr.formatDate(startDate, "Y-m-d");
-                document.getElementById('end_date').value = flatpickr.formatDate(endDate, "Y-m-d");
-                const rangoFechasTexto = document.getElementById('rango-fechas-texto');
-                rangoFechasTexto.innerHTML = `<strong>de:</strong> ${flatpickr.formatDate(startDate, "d-m-Y")} <strong>hasta:</strong> ${flatpickr.formatDate(endDate, "d-m-Y")}`;
+    function mostrarCalendario(fechasReservadas){
+        flatpickr("#rango-fechas", {
+            mode: "range",
+            dateFormat: "Y-m-d",
+            inline: true,
+            locale: {
+                rangeSeparator: ' a: '
+            },
+            disable: fechasReservadas,
+            onChange: function(selectedDates) {
+                if (selectedDates.length === 2) {
+                    const startDate = selectedDates[0];
+                    const endDate = selectedDates[1];
+                    document.getElementById('start_date').value = flatpickr.formatDate(startDate, "Y-m-d");
+                    document.getElementById('end_date').value = flatpickr.formatDate(endDate, "Y-m-d");
+                    const rangoFechasTexto = document.getElementById('rango-fechas-texto');
+                    rangoFechasTexto.innerHTML = `<strong>de:</strong> ${flatpickr.formatDate(startDate, "d-m-Y")} <strong>hasta:</strong> ${flatpickr.formatDate(endDate, "d-m-Y")}`;
+                }
             }
-        }
-    });
+        });
+    }
 
     // manejo de alertas de error/exito al ejecutar un nuevo alquiler
     let successMessage = document.getElementById("alquiler-success")
