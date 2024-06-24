7c5b04d930660d5b44f92d897d16d23523e1034e
camilamarendazzo <camimarendazzo@gmail.com>
2024-06-23 22:04:27 -0300

    Update pago.js

--- src/main/webapp/resources/core/js/pago.js
+++ src/main/webapp/resources/core/js/pago.js
@@ -1,4 +1,7 @@
-const mp = new MercadoPago('YOUR_PUBLIC_KEY');
+const mp = new MercadoPago("APP_USR-ce16a581-25f1-479c-b2bb-db5ad1368dc9", {
+    locale: "es-AR", // The most common are: 'pt-BR', 'es-AR' and 'en-US'
+});
+
 const bricksBuilder = mp.bricks();
 
 mp.bricks().create("wallet", "wallet_container", {
